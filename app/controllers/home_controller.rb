ca1bc287dd0d8166ded7e4e5ac55b0ced2e89620
Lev Lukomsky <lllukom@gmail.com>
2013-03-09 19:55:15 +0200

    moved to windows; added riddles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 class HomeController < ApplicationController
   BANNED_TWO_LETTER_WORDS = %w(Ям аа ма ра юл іо)
   def two_letter_words
@@ -15,5 +16,25 @@ class HomeController < ApplicationController
 
   def four_letter_words
     @words = Word.where('LENGTH(word) = 8').all
+  end
+
+  def riddles
+    @words = Word.where(%q(LENGTH(word) = 12 AND word LIKE '%ґ%')).all
+    @words.map!(&:word)
+    @words.select! { |w| w.include?('ґ') }
+    @words_riddles = Array.new(3).map { words_riddles(@words) }
+  end
+
+  private
+
+  def words_riddles(words)
+    words.shuffle.map { |w| word_riddle(w) }
+  end
+
+  def word_riddle(word)
+    chars = word.chars.to_a
+    chars += ScrabbleUtils.letters_array.sample(7 - word.size) if chars.size < 7
+    chars.shuffle!
+    chars.join('&nbsp;')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Scrabble::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#four_letter_words'
+  root :to => 'home#riddles'
 
   # See how all your routes lay out with "rake routes"
 
